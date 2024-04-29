6e405b25d51a47534dfca301e8b107ccf9ba4dfb
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-04-28 23:53:07 -0400

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+
+#if you’re using function views and not class views. For class views:
+#from rest_framework.views import APIView
+
+# Create your views here.
